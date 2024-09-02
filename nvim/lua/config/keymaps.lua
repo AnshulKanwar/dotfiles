68c4501c190b4d8f5040121dd73fdd05dd466b62
Anshul <anshul.kanwar@adityabirla.com>
2024-09-02 15:44:30 +0530

    Telescope config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,21 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>jf', builtin.git_files, {})
+
+local is_inside_work_tree = {}
+local project_files = function()
+	local cwd = vim.fn.getcwd()
+  	if is_inside_work_tree[cwd] == nil then
+    		vim.fn.system("git rev-parse --is-inside-work-tree")
+    		is_inside_work_tree[cwd] = vim.v.shell_error == 0
+  	end
+
+	if is_inside_work_tree[cwd] then
+    		builtin.git_files()
+  	else
+    		builtin.find_files()
+  	end
+end
+
+vim.keymap.set('n', '<leader>ff', project_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,28 @@
+local config = function()
+	local builtin = require("telescope.builtin")
+	local telescope = require("telescope")
+
+	telescope.setup({
+		defaults = {
+			file_ignore_patterns = { "^node_modules/" },
+			path_display = { "truncate" },
+			pickers = {
+				find_files = {
+		  			hidden = true,
+				},
+	      		},
+		},
+	})
+
+	--telescope.load_extension("fzf")
+	
+end
+
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
 	    'nvim-lua/plenary.nvim',
 	    'nvim-tree/nvim-web-devicons'
-    }
+    },
+    config = config
 }
